b70538a63b02b4779677943f82fa05b5be61a3cb
Caroline-echev <coraline.e1994@gmail.com>
2024-03-30 00:53:43 -0500

    Test units

--- src/test/java/com/bootcamp/onclass/domain/api/usecase/BootcampUseCaseTest.java
+++ src/test/java/com/bootcamp/onclass/domain/api/usecase/BootcampUseCaseTest.java
@@ -107,4 +107,71 @@ class BootcampUseCaseTest {
 
 
     }
+
+    @Test
+    @DisplayName("Expected list of bootcamps to be returned")
+    void shouldGetAllBootcamps() {
+        // GIVEN
+
+        List<Technology> technologies = new ArrayList<>();
+        technologies.add(new Technology(1L, "Java", "Lenguaje robusto para desarrollo backend"));
+        technologies.add(new Technology(2L, "Node.js", "Entorno para construir servidores escalables en JavaScript"));
+        technologies.add(new Technology(3L, "Spring Boot", "Framework Java para desarrollo rápido de aplicaciones"));
+
+        List<Technology> technologies1 = new ArrayList<>();
+        technologies1.add(new Technology(4L, "React.js", "Biblioteca para interfaces de usuario interactivas"));
+        technologies1.add(new Technology(5L, "Angular", "Framework para aplicaciones web robustas"));
+        technologies1.add(new Technology(3L, "JavaScript", "Agrega interactividad a las páginas web"));
+
+        List<Capacity> capacities = new ArrayList<>();
+        capacities.add (new  Capacity(1L,
+                "Desarrollador Backend",
+                "Diseño y construcción de la lógica y funcionalidades de la parte del servidor de una aplicación",
+                technologies));
+        capacities.add (new  Capacity(2L,
+                "Desarrollador Frontend",
+                "Creación de la interfaz de usuario y experiencia de usuario de una aplicación web o móvil",
+                technologies1));
+
+
+
+        List<Technology> technologies2 = new ArrayList<>();
+        technologies.add(new Technology(1L, "Java", "Lenguaje robusto para desarrollo backend"));
+        technologies.add(new Technology(2L, "Node.js", "Entorno para construir servidores escalables en JavaScript"));
+        technologies.add(new Technology(3L, "Spring Boot", "Framework Java para desarrollo rápido de aplicaciones"));
+
+        List<Technology> technologies3 = new ArrayList<>();
+        technologies1.add(new Technology(4L, "React.js", "Biblioteca para interfaces de usuario interactivas"));
+        technologies1.add(new Technology(5L, "Angular", "Framework para aplicaciones web robustas"));
+        technologies1.add(new Technology(3L, "JavaScript", "Agrega interactividad a las páginas web"));
+
+        List<Capacity> capacities1 = new ArrayList<>();
+        capacities.add (new  Capacity(3L,
+                "Desarrollador Backend",
+                "Diseño y construcción de la lógica y funcionalidades de la parte del servidor de una aplicación",
+                technologies));
+        capacities.add (new  Capacity(4L,
+                "Desarrollador Frontend",
+                "Creación de la interfaz de usuario y experiencia de usuario de una aplicación web o móvil",
+                technologies1));
+
+        List<Bootcamp> bootcamps = new ArrayList<>();
+        bootcamps.add (new Bootcamp(1L, "Desarrollo Full Stack",
+                "Conviértete en un desarrollador versátil capaz de crear tanto la lógica del servidor como las interfaces de usuario interactivas",
+                capacities));
+        bootcamps.add (new Bootcamp(2L, "Desarrollo Full Stack",
+                "Conviértete en un desarrollador versátil capaz de crear tanto la lógica del servidor como las interfaces de usuario interactivas",
+                capacities1));
+
+
+
+        //WHEN
+        when(bootcampPersistencePort.getAllBootcamps(anyInt(), anyInt(), anyBoolean(),anyBoolean()))
+                .thenReturn(bootcamps);
+        List<Bootcamp> actualBootcamps = bootcampUseCase.getAllBootcamps(0, 10, true, true);
+
+        // THEN
+        assertEquals(bootcamps.size(), actualBootcamps.size());
+    }
+
 }
